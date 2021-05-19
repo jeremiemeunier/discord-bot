e8ab6107b7445dbc6039699fd8b94736acf527c6
Bichon Codeurs <jeremie.meunier74@gmail.com>
2021-05-19 16:46:03 +0200

    Update bot.js
    Update version to v0.3

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ const client = new CommandoClient({
     disableMentions: 'everyone',
     presence: {
       activity: {
-        name: 'son maître | v0.2 | !help',
+        name: 'son maître | v0.3 | !help',
         type: 'LISTENING'
       }
     }
