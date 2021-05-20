0d2bc2e6e9a8efc540becb60327da304f0bcbbd1
Bichon Codeurs <jeremie.meunier74@gmail.com>
2021-05-20 19:15:56 +0200

    Update twitchlive.js

--- twitchlive.js
+++ twitchlive.js
@@ -11,15 +11,16 @@ for (const k in envConfig) {
 
 var json_settings = fs.readFileSync('booty.json');
 var booty_settings = JSON.parse(json_settings);
+var bootyLog = `\x1b[32m bootyJS[${booty_settings.version}] \x1b[0m`;
 
 console.log(`\x1b[42m\x1b[30m ### booty JS  [${booty_settings.version}] INITIALIZE ### \x1b[0m `);
 
 if(booty_settings.debug == true) {
-	console.log(`\x1b[32m bootyJS[${booty_settings.version}]\x1b[0m \x1b[43m\x1b[30m ### DEBUG IS ENABLED ### \x1b[0m `);
-	console.log(`\x1b[32m bootyJS[${booty_settings.version}]\x1b[0m using \x1b[34m${booty_settings.channel.debug}\x1b[0m channel to send his messages`);
+	console.log(`${bootyLog} \x1b[43m\x1b[30m ### DEBUG IS ENABLED ### \x1b[0m `);
+	console.log(`${bootyLog} using \x1b[34m${booty_settings.channel.debug}\x1b[0m channel to send his messages`);
 } else {
-	console.log(`\x1b[32m bootyJS[${booty_settings.version}]\x1b[0m \x1b[43m\x1b[30m ### DEBUG IS DISABLED ### \x1b[0m `);
-	console.log(`\x1b[32m bootyJS[${booty_settings.version}]\x1b[0m using \x1b[34m${booty_settings.channel.announce}\x1b[0m channel to send his messages`);
+	console.log(`${bootyLog} \x1b[43m\x1b[30m ### DEBUG IS DISABLED ### \x1b[0m `);
+	console.log(`${bootyLog} using \x1b[34m${booty_settings.channel.announce}\x1b[0m channel to send his messages`);
 }
 
 
@@ -43,11 +44,22 @@ function xhrCheck(xhr) {
 	else { return false; }
 }
 
+function dateReturn(ajd) {
+	let ret;
+
+	if(ajd.getHours() < 10) { ret += `0${ajd.getHours()}:`; } else { ret += `${ajd.getHours()}:`; }
+	if(ajd.getMinutes() < 10) { ret += `0${ajd.getMinutes()}:`; } else { ret += `${ajd.getMinutes()}:`; }
+	if(ajd.getSeconds() < 10) { ret += `0${ajd.getSeconds()}:`; } else { ret += `${ajd.getSeconds()}:`; }
+
+	return ret;
+}
+
 function isonliveid(data) {
 	let server = client.guilds.cache.get(process.env.GUILD_ID);
 	let announce = client.channels.cache.find(channel => channel.name === booty_settings.channel.announce)
 	let logs = client.channels.cache.find(channel => channel.name === booty_settings.channel.debug)
 	let every = server.roles.cache.find(role => role.name === '@everyone');
+	let dateajd = new Date();
 	
 	let twitchData = new XMLHttpRequest();
 	let twitchAuth = new XMLHttpRequest();
@@ -57,39 +69,38 @@ function isonliveid(data) {
 	twitchAuth.onreadystatechange = function(e) {
 		if(xhrCheck(twitchAuth)) {
 			if(booty_settings.debug == true) {
-				console.log(`\x1b[32m bootyJS[${booty_settings.version}] \x1b[0mAUTH : [\x1b[32mDONE\x1b[0m  (STATUS : ${twitchAuth.status})(STATE : ${twitchAuth.readyState})]`); }
-			let twitchCode = JSON.parse(twitchAuth.responseText); // on récupère les infos
+				console.log(`\x1b[32m bootyJS[${booty_settings.version}] ${dateReturn(dateajd)} \x1b[0mAUTH : [\x1b[32mDONE\x1b[0m  (STATUS : ${twitchAuth.status})(STATE : ${twitchAuth.readyState})]`); }
+			let twitchCode = JSON.parse(twitchAuth.responseText);
 
 			twitchData.onreadystatechange = function() {
 				if(xhrCheck(twitchData) === true) {
 					if(booty_settings.debug == true) {
-						console.log(`\x1b[32m bootyJS[${booty_settings.version}] \x1b[0mDATA : [\x1b[32mDONE\x1b[0m  (STATUS : ${twitchData.status})(STATE : ${twitchData.readyState})]`); }
+						console.log(`\x1b[32m bootyJS[${booty_settings.version}] ${dateReturn(dateajd)} \x1b[0mDATA : [\x1b[32mDONE\x1b[0m  (STATUS : ${twitchData.status})(STATE : ${twitchData.readyState})]`); }
 					let twitchResponse = JSON.parse(twitchData.responseText).data[0];
 
 					if(twitchResponse != undefined){
-                        // [ONLINE] si le rôle y est on ne fait rien sinon on le donne à l'utilisateur
 						if(booty_settings.debug == true) {
-							console.log(`\x1b[32m bootyJS[${booty_settings.version}] \x1b[0mETAT : [\x1b[32mONLINE \x1b[0m (${data.twitch_id})]`); }
+							console.log(`\x1b[32m bootyJS[${booty_settings.version}] ${dateReturn(dateajd)} \x1b[0mETAT : [\x1b[32mONLINE \x1b[0m (${data.twitch_id})]`); }
 						let streamClock = Date.parse(twitchResponse.started_at);
 
 						let now = Date.parse(new Date());
-						let pre = now - 300000; // On retire 5 minutes
-						let nex = now + 300000; // On ajoute 5 minutes
+						let pre = now - booty_settings.countdown;
+						let nex = now + booty_settings.countdown;
 
 						if(streamClock > pre && streamClock < nex) {
 							if(booty_settings.debug == true) {
-								console.log(`\x1b[32m bootyJS[${booty_settings.version}] \x1b[0mEnvoie le message`);
+								console.log(`\x1b[32m bootyJS[${booty_settings.version}] ${dateReturn(dateajd)} \x1b[0mEnvoie le message`);
 								logs.send(`Hey <@${every.id}> ! <@${data.discord_id}> est actuellement en live sur https://twitch.tv/${data.twitch_name} il stream **${twitchResponse.title}** sur ${twitchResponse.game_name}`);
 							}
 							else {
 								announce.send(`Hey <@${every.id}> ! <@${data.discord_id}> est actuellement en live sur https://twitch.tv/${data.twitch_name} il stream **${twitchResponse.title}** sur ${twitchResponse.game_name}`);
 							}
 						}
-						else { console.log(`\x1b[32m bootyJS[${version}] \x1b[0mPas de message`); }
+						else { console.log(`\x1b[32m bootyJS[${version}] ${dateReturn(dateajd)} \x1b[0mPas de message`); }
                     }
                     else {
 						if(booty_settings.debug == true) {
-							console.log(`\x1b[32m bootyJS[${booty_settings.version}] \x1b[0mETAT : [\x1b[31mOFFLINE \x1b[0m (${data.twitch_id})]`); }
+							console.log(`\x1b[32m bootyJS[${booty_settings.version}] ${dateReturn(dateajd)} \x1b[0mETAT : [\x1b[31mOFFLINE \x1b[0m (${data.twitch_id})]`); }
                     }
 				}
 			}
@@ -104,21 +115,22 @@ function isonliveid(data) {
 }
 
 client.on('ready', () => {
-	console.log(`\x1b[32m bootyJS[${booty_settings.version}]\x1b[0m as \x1b[34m${client.user.tag}\x1b[0m`);
+	console.log(`${bootyLog} as \x1b[34m${client.user.tag}\x1b[0m`);
 	if(booty_settings.waiting == true) {
-		console.log(`\x1b[32m bootyJS[${booty_settings.version}]\x1b[0m countdown : \x1b[34m${booty_settings.countdown}ms\x1b[0m`);
-		console.log(`\x1b[32m bootyJS[${booty_settings.version}]\x1b[0m as now starting countdown`);
+		console.log(`${bootyLog} countdown : \x1b[34m${booty_settings.countdown}ms\x1b[0m`);
+		console.log(`${bootyLog} as now starting countdown`);
 	}
-	else { console.log(`\x1b[32m bootyJS[${booty_settings.version}]\x1b[0m is to exited to wait`); }
+	else { console.log(`${bootyLog} is to exited to wait`); }
 	console.log(`\x1b[42m\x1b[30m ### booty JS  [${booty_settings.version}] STARTING ### \x1b[0m `);
 	
 	if(booty_settings.waiting == true) {
 		setInterval(function() {
-			console.log(`\x1b[32m bootyJS[${booty_settings.version}]\x1b[0m as waiting ${booty_settings.countdown}ms`);
-			console.log(`\x1b[32m bootyJS[${booty_settings.version}]\x1b[0m as starting his work`);
+			let ajd = new Date();
+			console.log(`\x1b[32m bootyJS[${booty_settings.version}] ${dateReturn(ajd)}\x1b[0m as waiting \x1b[34m${booty_settings.countdown}ms\x1b[0m`);
+			console.log(`\x1b[32m bootyJS[${booty_settings.version}] ${dateReturn(ajd)}\x1b[0m as starting his work`);
 			discordBotLive();
-			console.log(`\x1b[32m bootyJS[${booty_settings.version}]\x1b[0m as ending his work`);
-			console.log(`\x1b[32m bootyJS[${booty_settings.version}]\x1b[0m as now starting countdown`);
+			console.log(`\x1b[32m bootyJS[${booty_settings.version}] ${dateReturn(ajd)}\x1b[0m as ending his work`);
+			console.log(`\x1b[32m bootyJS[${booty_settings.version}] ${dateReturn(ajd)}\x1b[0m as now starting countdown`);
 		}, booty_settings.countdown);
 	}
 	else { discordBotLive(); }
